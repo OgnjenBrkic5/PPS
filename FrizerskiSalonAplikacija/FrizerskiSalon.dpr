3e0c7a5af289e4c60d125c767eb11e7ce35c3beb
igordizdarevic99@gmail.com <IgorDizdarevic99>
2024-04-18 16:31:41 +0200

    Kreiranje forme Informacije

--- FrizerskiSalonAplikacija/FrizerskiSalon.dpr
+++ FrizerskiSalonAplikacija/FrizerskiSalon.dpr
@@ -8,7 +8,8 @@ uses
   FormaTermini in 'FormaTermini.pas' {Form3},
   LogInForma in 'LogInForma.pas' {Form5},
   Prijava in 'Prijava.pas' {Form7},
-  Registracija in 'Registracija.pas' {Form6};
+  Registracija in 'Registracija.pas' {Form6},
+  Informacije in 'Informacije.pas' {Form4};
 
 {$R *.res}
 
@@ -20,5 +21,6 @@ begin
   Application.CreateForm(TForm5, Form5);
   Application.CreateForm(TForm7, Form7);
   Application.CreateForm(TForm6, Form6);
+  Application.CreateForm(TForm4, Form4);
   Application.Run;
 end.
